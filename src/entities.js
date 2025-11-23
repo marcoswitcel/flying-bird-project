b082322f9de3bc139726a600f14e6de45a5bd06f
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-11-22 23:42:53 -0300

    .

---
+++ src/entities.js
@@ -0,0 +1,65 @@
+import { Camera } from './camera.js';
+import { CollisionShape, RectCollisionShape } from './collision.js';
+import { resourceManager } from './resource-manager.js';
+import { Sprite } from './sprite.js';
+import { vec2, Vector2 } from './vector2.js';
+
+export class Entity {
+  type = 'Entity';
+  centered = true;
+  position = vec2();
+  /**
+   * @type {CollisionShape|null}
+   */
+  collisionShape = null;
+
+  /**
+   * @type {Sprite|null}
+   */
+  sprite = null;
+  /**
+   * @type {Vector2}
+   */
+  dimension = vec2(0, 0);
+
+  /**
+   * 
+   * @param {Camera} camera 
+   */
+  isVisible(camera) {
+    // @todo João, não funciona
+    return (this.position.x >= (camera.position.x - camera.dimensions.x / 2) && this.position.x <= (camera.position.x + camera.dimensions.x / 2)) &&
+      (this.position.y >= (camera.position.y - camera.dimensions.y / 2) && this.position.y <= (camera.position.y + camera.dimensions.y / 2));
+  }
+
+  render(ctx, camera) {
+    if (!this.sprite) return;
+
+    const x = (this.position.x) - camera.position.x + ctx.canvas.width / 2;
+    const y = (this.position.y) - camera.position.y + ctx.canvas.height / 2;
+
+    this.sprite.render(ctx, { x: x, y: y }, this.dimension, this.centered);
+  }
+}
+
+export class PipeEntity extends Entity {
+  type = 'PipeEntity';
+  collisionShape = new RectCollisionShape();
+
+  constructor() {
+    super();
+    this.sprite = resourceManager.getSprite('pipe');
+  }
+}
+
+export class BirdEntity extends Entity {
+  type = 'BirdEntity';
+  collisionShape = new RectCollisionShape();
+  velocity = vec2(0, 0);
+  accel = vec2(0, 0);
+
+  constructor() {
+    super();
+    this.collisionShape.color = 'yellow';
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { Camera } from './camera.js';
 import { CollisionShape, RectCollisionShape } from './collision.js';
+import { BirdEntity, Entity, PipeEntity } from './entities.js';
 import { drawRect, drawRectStroke } from './render.js';
 import { resourceManager } from './resource-manager.js';
 import { Sprite } from './sprite.js';
@@ -36,67 +37,6 @@ pipeImg.onload = () => {
 
 resourceManager.add('./assets/image/pipe/pipe.svg', 'image','pipe');
 
-
-
-class Entity {
-  type = 'Entity';
-  centered = true;
-  position = vec2();
-  /**
-   * @type {CollisionShape|null}
-   */
-  collisionShape = null;
-
-  /**
-   * @type {Sprite|null}
-   */
-  sprite = null;
-  /**
-   * @type {Vector2}
-   */
-  dimension = vec2(0, 0);
-
-  /**
-   * 
-   * @param {Camera} camera 
-   */
-  isVisible(camera) {
-    // @todo João, não funciona
-    return (this.position.x >= (camera.position.x - camera.dimensions.x / 2) && this.position.x <= (camera.position.x + camera.dimensions.x / 2)) &&
-      (this.position.y >= (camera.position.y - camera.dimensions.y / 2) && this.position.y <= (camera.position.y + camera.dimensions.y / 2));
-  }
-
-  render(ctx, camera) {
-    if (!this.sprite) return;
-
-    const x = (this.position.x) - camera.position.x + ctx.canvas.width / 2;
-    const y = (this.position.y) - camera.position.y + ctx.canvas.height / 2;
-
-    this.sprite.render(ctx, { x: x, y: y }, this.dimension, this.centered);
-  }
-}
-
-class PipeEntity extends Entity {
-  type = 'PipeEntity';
-  collisionShape = new RectCollisionShape();
-
-  constructor() {
-    super();
-    this.sprite = resourceManager.getSprite('pipe');
-  }
-}
-
-class BirdEntity extends Entity {
-  type = 'BirdEntity';
-  collisionShape = new RectCollisionShape();
-  velocity = vec2(0, 0);
-  accel = vec2(0, 0);
-
-  constructor() {
-    super();
-    this.collisionShape.color = 'yellow';
-  }
-}
 
 const camera = new Camera();
 camera.position.x = ctx.canvas.width / 2;
