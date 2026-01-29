53d34855580fa4a2e4c1e059a32002025d63aa2a
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-01-28 22:46:29 -0300

    WIP: grama de exemplo

--- src/main.js
+++ src/main.js
@@ -23,6 +23,8 @@ resourceManager.add('./assets/image/bird/idle/frame-3.png', 'image','bird.3');
 resourceManager.add('./assets/image/bird/idle/frame-4.png', 'image','bird.4');
 resourceManager.add('./assets/image/bird/got-hit/frame-1.png', 'image','bird.hit');
 resourceManager.add('./assets/image/floor/floor.svg', 'image','floor');
+resourceManager.add('./assets/image/floor/grass-1.svg', 'image','grass.1');
+resourceManager.add('./assets/image/floor/grass-2.svg', 'image','grass.2');
 // parallax
 resourceManager.add('./assets/image/parallax/mountains.svg', 'image','mountains');
 resourceManager.add('./assets/image/parallax/clouds.svg', 'image','clouds');
