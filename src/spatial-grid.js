1a0ce7ed7c8badd9beff9a3bea31ce6ae0975b99
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-02-11 22:12:23 -0300

    .

---
+++ src/spatial-grid.js
@@ -0,0 +1,52 @@
+import { Entity } from './entities.js';
+
+/**
+ * @todo João, terminar de implementar os métodos
+ * @todo João, implementar o grid em paralelo com o código atual e validar se o
+ * código atual e o grid encontram as mesmas entidades. Vai ser necessário ajustar 
+ * o código atual para adicionar as entidades renderizada a uma lista para fins de comparação.
+ */
+export class SpatialGrid {
+  /**
+   * @type {Map<number, Entity>}
+   */
+  grid;
+
+  /**
+   * @type {number}
+   */
+  cellSize;
+
+  /**
+   * 
+   * @param {number} cellSize 
+   */
+  constructor(cellSize) {
+    this.cellSize = cellSize;
+    this.grid = new Map();
+  }
+
+    
+  insert(entity) {
+    // @todo João, implementar
+  }
+
+  update(entity) {
+    // @todo João, implementar
+  }
+
+  remove(entity) {
+    // @todo João, implementar
+  }
+
+  /**
+   * 
+   * @param {number} x 
+   * @param {number} y 
+   * @param {number} width 
+   * @param {number} height 
+   */
+  query(x, y, width, height) {
+    // @todo João, implementar
+  }
+}
